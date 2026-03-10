71497a3f9fc40e298d7466bb2b48e3ef9126fd6c
Finlay MacLean <finlaymaclean@Mac.communityfibre.co.uk>
2026-03-10 17:53:11 +0000

    Initial commit

--- src/rdsl/__init__.py
+++ src/rdsl/__init__.py
@@ -1,9 +1,18 @@
+import sys
+
+from loguru import logger
+
 from rdsl.functional_groups import (
     get_all_functional_group_patterns,
     get_functional_group_matches,
 )
 from rdsl.highlight import highlight_atoms
 from rdsl.select import SelectionResult, select_atom_ids, select_atoms, select_molecule
+
+# Configure loguru to use INFO level by default
+logger.remove()
+logger.add(sys.stderr, level="INFO")
+
 
 __all__ = [
     "SelectionResult",
